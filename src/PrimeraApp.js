3bff78ccfc089092fe22a1027738f2a5f4a43da4
Ezequiel Orsingher <ezequielorsingher@gmail.com>
2023-12-11 22:47:25 -0300

    Returning component as an object

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,8 +3,15 @@ import React from "react";
 
 const PrimeraApp = () => {
 
-    return <h1>hola mundo</h1>
-
+    // Se utiliza un paréntesis para indicar que todo lo que le sigue al mismo es un objeto.
+    //De esta manera puedo retornar varias lineas, sino solamente retornaría la primera ya que interpreta un ";" al finalizar la misma.
+    return (
+        <div>
+            <h1>hola mundo</h1>
+            <p>Mi primera app</p>
+        </div>
+        //Nota: Este div se pone porque se debe retornar 1 solo objeto en un componente SIEMPRE. Tambien puedo usar un <> o fragment. Ampliar
+    );
 }
 
 export default PrimeraApp;
