9411aff5946260addaff7fe62d5bd7ffe1c260e8
Ezequiel Orsingher <ezequielorsingher@gmail.com>
2023-12-26 07:34:45 -0300

    propTypes and defaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,8 @@
 //Funcional components > componentes como funcion
 import React from "react";
+import propTypes from 'prop-types'
 //Destructuring PrimeraApp. We no longer need to have the const *props* to call *saludo* property since the main component is sending the props.
-const PrimeraApp = ({ saludo = "Hola mundo"/*We can add a default value in the case that no property is sent from the component*/}) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
      const objeto = {
          name: "eze",
@@ -15,10 +16,18 @@ const PrimeraApp = ({ saludo = "Hola mundo"/*We can add a default value in the c
         <div>
             <h1>{ saludo }</h1>
             <pre>{JSON.stringify(objeto)}</pre> 
-            <p>Mi primera app</p>
+            <p>{ subtitulo }</p>
         </div>
         //Nota: Este div se pone porque se debe retornar 1 solo objeto en un componente SIEMPRE. Tambien puedo usar un <> o fragment. Ampliar
     );
 }
 
+//Load prop types. It's used to assign properties. For example, here we are saying that the 'saludo' must be a string and is required to have a value. 
+PrimeraApp.propTypes = {
+    saludo: propTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: '' //assign a default value to a property (another way)
+}
 export default PrimeraApp;
